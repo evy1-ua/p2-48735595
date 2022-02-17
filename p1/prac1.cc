d752902c0f97172447cc5c0d43256fd7cd504e8a
evy1-ua <evy1@alu.ua.es>
2022-02-17 01:30:37 +0100

    Modulo creacion de enemigo y heroe completado

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -9,9 +9,10 @@
 
 using namespace std;
 
-const int KNAME=32;
-const int KENEMIES=5;
-const int KPOINTS=200;
+//DECLARACIÓN DE CONSTANTES
+const int KNAME=32; //Máximo numero de caracteres del nombre del héroe
+const int KENEMIES=5; //Número de razas de enemigos diferentes
+const int KPOINTS=200; //Número de puntos de habilidad a repartir
 const int KDICE=20; // Número de caras del dado
 
 struct Core{
@@ -27,6 +28,7 @@ enum Breed{
   HELLHOUND,
   DRAGON
 };
+//Enumeramos los errores
 enum Error{
   ERR_NAME,
   ERR_DISTRIBUTION,
@@ -47,8 +49,7 @@ struct Hero{
   int exp;
   int kills[KENEMIES];
 };
-
-
+//Función que recibe un enum y imprimirá por pantalla el error
 void error(Error e){
   switch(e){
     case ERR_NAME:
@@ -69,11 +70,11 @@ void error(Error e){
 int rollDice(){
   return rand()%KDICE+1;
 }
-//Modulo de introducir el nombre del héroe//
+//Modulo de introducir el nombre del héroe, recibirá un parámetro de heroe
 void Heroname(Hero& hero){
 int i=0;
 do{
-  cout<<"Enter hero name: \n";
+  cout<<"Enter hero name: ";
   fgets(hero.name,KNAME,stdin);
   while(hero.name[i]){
     if(isalpha(hero.name[0])==0 || ispunct(hero.name[i])){
@@ -86,14 +87,126 @@ do{
   }
 }while(isalpha(hero.name[0])==0 || ispunct(hero.name[i])!=0) ;
 }
+void Heropoints(Hero& hero){
+unsigned int total,pos=0;
+float attack_distr,defense_distr;
+do{
+  string aux="",distribution="";
+  unsigned int i=0;
+  attack_distr=0;
+  defense_distr=0;
+  hero.features.attack=0;
+  hero.features.defense=0;
+  hero.features.hp=0;
+  hero.special=false;
+  hero.exp=0;
+  hero.kills[KENEMIES]=0;
+
+    cout<<"Enter attack/defense: ";
+    getline(cin,distribution);
+      pos=distribution.find("/");
+      if(pos>=0){
+        for( i=0;i<pos;i++){
+        aux+=distribution[i];
+      }
+        attack_distr=(atoi(aux.c_str()));
+        hero.features.attack=KPOINTS*(attack_distr/100);
+        aux="";
+       for( i=pos+1;i<distribution.length();i++){
+          aux+=distribution[i];
+       }
+        defense_distr=(atof(aux.c_str()));
+        hero.features.defense=KPOINTS*(defense_distr/100);
+        hero.features.hp=2*hero.features.defense;
+        total=attack_distr+defense_distr;
+        hero.special=true;
+        if(attack_distr<0 || defense_distr<0 || total!=100){
+          error(ERR_DISTRIBUTION);
+        }
+      }
+      else{
+        error(ERR_DISTRIBUTION);
+      }
+    }while(pos<0 || attack_distr<0 || defense_distr<0 || total!=100 );
+}
 Hero createHero(){
 Hero hero;
 Heroname(hero);
-cout<<hero.name;
+Heropoints(hero);
+return hero;
 }
-
+void imprimirEnemigo(Enemy& enemy){
+  string enemy_name;
+  switch (enemy.name)
+  {
+  case  0 :
+    enemy_name="Axolotl";
+    break;
+  case 1 :
+    enemy_name="Troll";
+    break;
+  case 2 :
+    enemy_name="Orc";
+    break;
+  case 3 :
+    enemy_name="Hellhound";
+    break;
+  default:
+    enemy_name="Dragon";
+    break;
+  }
+  cout<<"[Enemy]\n";
+  cout<<"Breed: "<<enemy_name<<endl;
+  cout<<"Attack: "<<enemy.features.attack<<endl;
+  cout<<"Defense: "<<enemy.features.defense<<endl;
+  cout<<"Health points: "<<enemy.features.hp<<endl;
+}
+void elegirenemigo(int dado,Enemy& enemy){
+  switch (dado)
+  {
+  case 1: case 2: case 3: case 4: case 5: case 6:
+    enemy.name=AXOLOTL;
+    enemy.features.attack=40;
+    enemy.features.defense=40;
+    enemy.features.hp=2*enemy.features.defense;
+    imprimirEnemigo(enemy);
+    break;
+  case 7: case 8: case 9: case 10: case 11:
+    enemy.name=TROLL;
+    enemy.features.attack=60;
+    enemy.features.defense=80;
+    enemy.features.hp=2*enemy.features.defense;
+    imprimirEnemigo(enemy);
+    break;
+  case 12: case 13: case 14: case 15:
+    enemy.name=ORC;
+    enemy.features.attack=80;
+    enemy.features.defense=120;
+    enemy.features.hp=2*enemy.features.defense;
+    imprimirEnemigo(enemy);
+    break;
+  case 16: case 17: case 18:
+    enemy.name=HELLHOUND;
+    enemy.features.attack=120;
+    enemy.features.defense=100;
+    enemy.features.hp=2*enemy.features.defense;
+    imprimirEnemigo(enemy);
+    break;
+  default:
+    enemy.name=DRAGON;
+    enemy.features.attack=160;
+    enemy.features.defense=140;
+    enemy.features.hp=2*enemy.features.defense;
+    imprimirEnemigo(enemy);
+    break;
+  }
+  
+}
 Enemy createEnemy(){
-
+Enemy enemy;
+int dado=rollDice();
+elegirenemigo(dado,enemy);
+return enemy;
 }
 void fight(Hero &hero,Enemy &enemy){
 }
@@ -118,6 +231,33 @@ int main(int argc,char *argv[]){
   else{
     srand(atoi(argv[1])); // Introducimos la semilla para generar números aleatorios
     createHero();
+    createEnemy();
+    char opc;
+    do{
+      showMenu();
+      cin>>opc;
+      switch (opc)
+      {
+      case '1':
+        cout<<"Fight";
+        break;
+      case '2':
+        cout<<"Run away";
+        break;
+      case '3':
+        cout<<"Special";
+        break;
+      case '4':
+        cout<<"Report";
+        break;
+      case 'q':
+        break;
+      default:
+      error(ERR_OPTION);
+        break;
+      }
+    }while(opc!='q');
+    
     // Aquí vendrá todo tu código del "main"...
     
   }
